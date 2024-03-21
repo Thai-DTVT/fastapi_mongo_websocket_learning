97bbd951e6c3dc1c63184210608b0a955ff51966
“MinhThai” <thaisinh1181998@gmail.com>
2024-03-21 21:24:50 +0700

    commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://thaisinh1181998:12345678pymongo@pymongo.lnt0axg.mongodb.net/?retryWrites=true&w=majority"
+uri = "mongodb+srv://thaisinh1181998:password@pymongo.lnt0axg.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
